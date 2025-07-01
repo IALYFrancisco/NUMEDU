5610a6e47d05813c1c4a063ffe6a3490ed22f2ce
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-01 14:56:05 +0300

    ajout d'une forme simple de formulaire dans la page de connexion

--- lib/screens/authentication/login.dart
+++ lib/screens/authentication/login.dart
@@ -15,7 +15,64 @@ class LoginPage extends StatelessWidget {
                     )
                 ),
                 child: Center(
-                    child: Text("Hello user, would you want to connect? Come back later.")
+                    child: SingleChildScrollView(
+                        padding: EdgeInsets.all(24),
+                        child: Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                            elevation: 10,
+                            child: Padding(
+                                padding: EdgeInsets.all(20),
+                                child: Form(
+                                    child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                            Text(
+                                                "hello!",
+                                                style: TextStyle(
+                                                    fontSize: 26,
+                                                    fontWeight: FontWeight.bold
+                                                )
+                                            ),
+                                            SizedBox(height: 20),
+                                            TextFormField(
+                                                decoration: InputDecoration(
+                                                    labelText: "Username"
+                                                )
+                                            ),
+                                            SizedBox(height: 10),
+                                            Row(
+                                                children: [
+                                                    Checkbox(value: true, onChanged: (v){}),
+                                                    Expanded(child: Text("I agree to the termes and conditions.")) 
+                                                ]
+                                            ),
+                                            SizedBox(height: 10),
+                                            ElevatedButton(
+                                                onPressed: () {},
+                                                style: ElevatedButton.styleFrom(
+                                                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16),
+                                                    shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(30) ),
+                                                    backgroundColor: Colors.blueAccent
+                                                ),
+                                                child: Text("Sign up"),
+                                            ),
+                                            SizedBox(height: 20),
+                                            Text("Log in with your social media account."),
+                                            SizedBox(height: 10),
+                                            Row(
+                                                mainAxisAlignment: MainAxisAlignment.center,
+                                                children: [
+                                                    IconButton( onPressed: (){}, icon: Icon(Icons.facebook), color: Colors.blue ),
+                                                    IconButton( onPressed: (){}, icon: Icon(Icons.g_mobiledata), color: Colors.blue ),
+                                                    IconButton( onPressed: (){}, icon: Icon(Icons.apple), color: Colors.black )
+                                                ]
+                                            )
+                                        ]
+                                    )
+                                )
+                            )
+                        )
+                    )
                 )
             )
         );
