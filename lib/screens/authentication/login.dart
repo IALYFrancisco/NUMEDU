99f42b6cc9bd964bb113f25e0700956a62a99e62
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-03 14:13:08 +0300

    développement de la page de connexion

--- lib/screens/authentication/login.dart
+++ lib/screens/authentication/login.dart
@@ -9,42 +9,163 @@ class LoginPage extends StatelessWidget {
         return Scaffold(
         backgroundColor: Colors.grey[900],
         body: Center(
-          child: SizedBox(
-            width: double.infinity,
-            height: double.infinity,
-            child: Stack(
-              children: [
-                ClipRRect(
-                  child: Image.asset(
-                    'assets/images/bg.jpg',
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                    height: double.infinity,
-                  ),
-                ),
-                ClipRRect(
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 2, sigmaY: 5),
-                    child: Container(
-                      width: double.infinity,
-                      height: double.infinity,
-                      color: Colors.black.withOpacity(0.2),
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: SizedBox(
+                width: double.infinity,
+                height: double.infinity,
+                child: Stack(
+                children: [
+                    ClipRRect(
+                    child: Image.asset(
+                        'assets/images/bg.jpg',
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        height: double.infinity,
                     ),
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    'Conteneur flouté',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
                     ),
-                  ),
+                    ClipRRect(
+                    child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 2, sigmaY: 5),
+                        child: Container(
+                        width: double.infinity,
+                        height: double.infinity,
+                        color: Colors.black.withOpacity(0.2),
+                        ),
+                    ),
+                    ),
+                    Center(
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                                Image.asset(
+                                    'assets/images/logo-de-numedu.png',
+                                    width: 80
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                    "Connexion",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold
+                                    )
+                                ),
+                                SizedBox(height: 50),
+                                Column(
+                                    children: [
+                                        Container(
+                                            width: 275,
+                                            child: Column(
+                                                children: [
+                                                    Row(
+                                                        children: [ 
+                                                            Text(
+                                                                "Votre email :",
+                                                                style: TextStyle(
+                                                                    color: Colors.white,
+                                                                    fontSize: 15
+                                                                )
+                                                            ),
+                                                        ]
+                                                    ),
+                                                    SizedBox(height: 5),
+                                                    TextField(
+                                                        decoration: InputDecoration(
+                                                            hintText: "ex: johndoe@example.com",
+                                                            isDense: true,
+                                                            filled: true,
+                                                            fillColor: Colors.white,
+                                                            contentPadding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 18.0),
+                                                            border: OutlineInputBorder(
+                                                                borderRadius: BorderRadius.circular(50.0),
+                                                                ),
+                                                                enabledBorder: OutlineInputBorder(
+                                                                borderRadius: BorderRadius.circular(50.0),
+                                                                borderSide: BorderSide(color: Colors.grey),
+                                                                ),
+                                                                focusedBorder: OutlineInputBorder(
+                                                                borderRadius: BorderRadius.circular(50.0),
+                                                                borderSide: BorderSide(color: const Color(0xFF23468E), width: 2),
+                                                                ),
+                                                        ),
+                                                        style: TextStyle(fontSize: 14, color: Colors.grey),
+                                                    ),
+                                                    SizedBox(height: 25),
+                                                    Row(
+                                                        children: [ 
+                                                            Text(
+                                                                "Votre mot de passe :",
+                                                                style: TextStyle(
+                                                                    color: Colors.white,
+                                                                    fontSize: 15
+                                                                )
+                                                            ),
+                                                        ]
+                                                    ),
+                                                    TextField(
+                                                        decoration: InputDecoration(
+                                                            hintText: ".......",
+                                                            isDense: true,
+                                                            filled: true,
+                                                            fillColor: Colors.white,
+                                                            contentPadding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 18.0),
+                                                            border: OutlineInputBorder(
+                                                                borderRadius: BorderRadius.circular(50.0),
+                                                                ),
+                                                                enabledBorder: OutlineInputBorder(
+                                                                borderRadius: BorderRadius.circular(50.0),
+                                                                borderSide: BorderSide(color: Colors.grey),
+                                                                ),
+                                                                focusedBorder: OutlineInputBorder(
+                                                                borderRadius: BorderRadius.circular(50.0),
+                                                                borderSide: BorderSide(color: const Color(0xFF23468E), width: 2),
+                                                                ),
+                                                        ),
+                                                        style: TextStyle(fontSize: 14, color: Colors.grey),
+                                                    ),
+                                                    SizedBox(height: 25),
+                                                    Row(
+                                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                        children: [
+                                                            ElevatedButton(
+                                                                onPressed: () {
+                                                                Navigator.push(
+                                                                    context,
+                                                                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                                                                );
+                                                                },
+                                                                style: ElevatedButton.styleFrom(
+                                                                shape: const StadiumBorder(),
+                                                                backgroundColor: const Color(0xFF23468E),
+                                                                foregroundColor: Colors.white,
+                                                                ),
+                                                                child: const Text(
+                                                                "Connexion",
+                                                                style: TextStyle(color: Colors.white),
+                                                                ),
+                                                            ),
+                                                            Text(
+                                                                "Haven't an account ?",
+                                                                style: TextStyle(
+                                                                    fontSize: 12,
+                                                                    color: Colors.white
+                                                                )
+                                                            )
+                                                        ]
+                                                    )
+                                                ]
+                                            )
+                                        )
+                                    ]
+                                )
+                            ]
+                        )
+                    )
+                ],
                 ),
-              ],
             ),
-          ),
+          ),  
         ),
       );
     }
