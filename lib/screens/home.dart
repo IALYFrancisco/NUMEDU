e159652bccb3e0c6ffb18fed0b25cfe77e537bbb
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-05 06:44:59 +0300

    page d'accueil redéveloppée

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,65 +1,153 @@
+// import 'package:flutter/material.dart';
+// import 'authentication/login.dart';
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         padding: const EdgeInsets.all(70),
+//         decoration: const BoxDecoration(
+//           image: DecorationImage(
+//             image: AssetImage('assets/images/bg.jpg'),
+//             fit: BoxFit.cover,
+//           ),
+//         ),
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             children: [
+//               const Column(),
+//               Column(
+//                 children: [
+//                   Image.asset('assets/images/logo-de-numedu.png', width: 80),
+//                   const SizedBox(height: 20),
+//                   const Text(
+//                     "Numédu",
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 24,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//               Column(
+//                 mainAxisAlignment: MainAxisAlignment.end,
+//                 children: [
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(builder: (context) => const LoginPage()),
+//                       );
+//                     },
+//                     style: ElevatedButton.styleFrom(
+//                       shape: const StadiumBorder(),
+//                       backgroundColor: const Color(0xFF23468E),
+//                       foregroundColor: Colors.white,
+//                     ),
+//                     child: const Text(
+//                       "Commencer",
+//                       style: TextStyle(color: Colors.white),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'authentication/login.dart';
+import 'dart:ui';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(70),
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/bg.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Column(),
-              Column(
-                children: [
-                  Image.asset('assets/images/logo-de-numedu.png', width: 80),
-                  const SizedBox(height: 20),
-                  const Text(
-                    "Numédu",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+        return Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: Center(
+          child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Stack(
+              children: [
+                ClipRRect(
+                  child: Image.asset(
+                    'assets/images/bg.jpg',
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: double.infinity,
+                  ),
+                ),
+                ClipRRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 2, sigmaY: 5),
+                    child: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      color: Colors.black.withOpacity(0.8),
                     ),
                   ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const LoginPage()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: const StadiumBorder(),
-                      backgroundColor: const Color(0xFF23468E),
-                      foregroundColor: Colors.white,
-                    ),
-                    child: const Text(
-                      "Commencer",
-                      style: TextStyle(color: Colors.white),
-                    ),
+                ),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Column(),
+                      Column(
+                        children: [
+                          Image.asset('assets/images/logo-de-numedu.png', width: 80),
+                          const SizedBox(height: 20),
+                          const Text(
+                            "Numédu",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const LoginPage()),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: const StadiumBorder(),
+                              backgroundColor: const Color(0xFF23468E),
+                              foregroundColor: Colors.white,
+                            ),
+                            child: const Text(
+                              "Commencer",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        SizedBox(height: 70)
+                        ],
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
