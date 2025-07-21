4cddaecd6d33c5d603babac63d255ebf1625c301
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-21 14:43:28 +0300

    corréction des configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   if (kIsWeb) {
     await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "TON_API_KEY",
-        authDomain: "TON_AUTH_DOMAIN",
-        projectId: "TON_PROJECT_ID",
-        storageBucket: "TON_STORAGE_BUCKET",
-        messagingSenderId: "TON_MESSAGING_SENDER_ID",
-        appId: "TON_APP_ID",
+      options: FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_API_KEY']!,
+        projectId: dotenv.env['FIREBASE_PROJECT_ID'],
+        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'],
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'],
+        appId: dotenv.env['FIREBASE_APP_ID'],
       ),
     );
   } else {
