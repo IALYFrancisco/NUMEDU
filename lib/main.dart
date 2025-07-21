e8fcb0115d9eae2f9fbe48c860b60abe7fac5e76
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-21 13:50:56 +0300

    définition de la méthode main() en async

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
