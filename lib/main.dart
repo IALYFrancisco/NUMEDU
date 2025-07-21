8018b16f5d251003dbc22176dbb928de2b1742f0
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-21 14:34:25 +0300

    mis à jour pour le teste dans le web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "TON_API_KEY",
+        authDomain: "TON_AUTH_DOMAIN",
+        projectId: "TON_PROJECT_ID",
+        storageBucket: "TON_STORAGE_BUCKET",
+        messagingSenderId: "TON_MESSAGING_SENDER_ID",
+        appId: "TON_APP_ID",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
